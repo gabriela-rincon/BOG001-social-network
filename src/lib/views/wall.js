734473417ec4001ad7fc34689bbe7880d9c16a88
dalejac <k-da929@hotmail.com>
2020-08-27 09:33:20 -0500

    Branch

---
+++ src/lib/views/wall.js
@@ -0,0 +1,5 @@
+let Wall = {
+
+}
+
+export default Wall;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,49 @@
 // Este es el punto de entrada de tu aplicacion
+import {Router} from './lib/index.js';
+import {Home} from './lib/views/home.js';
+import {Log} from './lib/views/log.js';
+// import LogIn from './lib/views/log.js';
+// import SignIn from './lib/views/sign.js';
+// import Profile from './lib/views/profile.js';
+// import Wall from './lib/views/wall.js';
 
-import { myFunction } from './lib/index.js';
+// const routes = {
+//     '/Home' : Home,
+//     '/LogIn' : LogIn,
+//     '/SignIn' : SignIn,
+//     '/Profile' : Profile,
+//     '/Wall' : Wall,
+// } 
 
-myFunction();
+let routes = {
+    Home: {
+        path: '/Home',
+        template: Home(),
+    },
+    LogIn: {
+        path: '/LogIn',
+        // template: Log()
+    },
+    SignIn: {
+        path: '/SignIn'
+    },
+    Profile: {
+        path: '/Profile'
+    },
+    Wall: {
+        path: '/Wall'
+    }, 
+};
+
+const ROUTER = new Router (routes);
+
+console.log(casa.name);
+
+
+console.log(ROUTER );
+
+
+
+
+
+
